eb57745a1d6dc59ef9ce15f12134252171f02eda
Mike Rose <michaelrose151@gmail.com>
2025-04-10 10:09:58 -0400

    Implement basic new inspection form

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -21,4 +21,17 @@ class InspectionsController < ApplicationController
   def new
     @inspection = Inspection.new
   end
+
+  def create
+    @inspection = Inspection.new(user_params)
+    if @inspection.save
+      redirect_to inspections_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.expect(inspection: [:title, :sheet_url_string, :active])
+  end
 end
