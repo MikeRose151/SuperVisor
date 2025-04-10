344968ae3adca5043022de370180fdc2b1dd299f
Mike Rose <michaelrose151@gmail.com>
2025-04-10 11:29:10 -0400

    Generate sheets controller and move table from inspections index to sheets index

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,21 +1,6 @@
 class InspectionsController < ApplicationController
   def index
     @inspections = Inspection.all
-
-    # TODO: move the following to a location that makes more sense
-    # TODO: ensure it's only sheets, atm it's all file types
-    drive = Google::Apis::DriveV3::DriveService.new
-    drive.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open(Rails.root.join("config", "credentials", "google_service_account.json")),
-      scope: "https://www.googleapis.com/auth/drive.readonly"
-    )
-
-    drive.authorization.fetch_access_token!
-
-    @files = drive.list_files(
-      q: "mimeType = 'application/vnd.google-apps.spreadsheet'",
-      fields: "files(id, name, mimeType, owners, webViewLink)"
-    )
   end
 
   def new
