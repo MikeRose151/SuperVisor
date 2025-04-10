ca1bc7c85a433c0d5905b30fb482bc037066ceec
Mike Rose <michaelrose151@gmail.com>
2025-04-10 11:34:07 -0400

    Move google api logic from sheets controller to a service

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,17 +1,6 @@
 class SheetsController < ApplicationController
   def index
-    drive = Google::Apis::DriveV3::DriveService.new
-    drive.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
-      json_key_io: File.open(Rails.root.join("config", "credentials", "google_service_account.json")),
-      scope: "https://www.googleapis.com/auth/drive.readonly"
-    )
-
-    drive.authorization.fetch_access_token!
-
-    @sheets = drive.list_files(
-      q: "mimeType = 'application/vnd.google-apps.spreadsheet'",
-      fields: "files(id, name, mimeType, owners, webViewLink)"
-    )
+    @sheets = GoogleDriveService.fetch_spreadsheets
   end
 
   def sync
---
+++ app/services/google_drive_service.rb
@@ -0,0 +1,16 @@
+class GoogleDriveService
+  def self.fetch_spreadsheets
+    drive = Google::Apis::DriveV3::DriveService.new
+    drive.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+      json_key_io: File.open(Rails.root.join("config", "credentials", "google_service_account.json")),
+      scope: "https://www.googleapis.com/auth/drive.readonly"
+    )
+
+    drive.authorization.fetch_access_token!
+
+    @sheets = drive.list_files(
+      q: "mimeType = 'application/vnd.google-apps.spreadsheet'",
+      fields: "files(id, name, mimeType, owners, webViewLink)"
+    )
+  end
+end
