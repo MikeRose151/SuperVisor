b4f1ab3f63fa3a780662c73939a933c9b7c06eda
Mike Rose <michaelrose151@gmail.com>
2025-04-11 14:24:14 -0400

    Update models logic

--- app/models/inspection.rb
+++ app/models/inspection.rb
@@ -1,3 +1,10 @@
 class Inspection < ApplicationRecord
-  enum status: { active: 0, archived: 1 }
+  belongs_to :sheet
+  
+  enum status: { active: 0, inactive: 1, archived: 2 }
+  # active => Inspection is in use
+  # inactive => Inspection is not in use (and its corresponding Sheet still exists)
+  # archived => corresponding Sheet no longer exists (e.g. deleted from google drive, no longer shared with SuperVisor) but user wants to preserve Inspection
+
+  validates :title, presence: true
 end
