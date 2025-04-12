f72fa183402066778305ec946218d3b66e309725
Mike Rose <michaelrose151@gmail.com>
2025-04-12 10:07:20 -0400

    Change variable name from google_drive_client to client

--- app/services/google_drive_service.rb
+++ app/services/google_drive_service.rb
@@ -1,14 +1,13 @@
 class GoogleDriveService
   def self.fetch_spreadsheets
-    google_drive_client = Google::Apis::DriveV3::DriveService.new
-    google_drive_client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
+    client = Google::Apis::DriveV3::DriveService.new
+    # TODO: move the following logic to a service as it's shared with google sheets
+    client.authorization = Google::Auth::ServiceAccountCredentials.make_creds(
       json_key_io: File.open(Rails.root.join("config", "credentials", "google_service_account.json")),
       scope: "https://www.googleapis.com/auth/drive.readonly"
     )
 
-    google_drive_client.authorization.fetch_access_token!
-
-    @sheets = google_drive_client.list_files(
+    @sheets = client.list_files(
       q: "mimeType = 'application/vnd.google-apps.spreadsheet'",
       fields: "files(id, name, mimeType, owners, webViewLink)"
     )
