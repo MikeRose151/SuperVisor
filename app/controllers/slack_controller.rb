f80b03ac1109060f71dff01aa2529ae2963ea933
Mike Rose <michaelrose151@gmail.com>
2025-04-26 09:49:14 -0400

    remove line that should be removed

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -4,7 +4,6 @@ class SlackController < ApplicationController
   def trigger_inspection
     @inspections = Inspection.all
     @sheets = Sheet.all
-    modal_payload = render_to_string(template: "slack/modals/inspect_form", formats: [ :json ]).html_safe
 
     modal_payload = SlackModalBuilder.build(inspections: @inspections, sheets: @sheets)
 
